d63b570371b26ba41e7f5b46af9f8174a5270e45
Miguel Diaz <miguel.diaz@balsain.io>
2025-11-05 13:00:57 +0100

    add metrics

--- ipfs/scripts/worker/020-config.sh
+++ ipfs/scripts/worker/020-config.sh
@@ -3,10 +3,15 @@ set -ex
 ipfs config --json AutoConf.Enabled false
 ipfs config --json AutoTLS.Enabled false
 ipfs config --json API.HTTPHeaders \
-    '{"Access-Control-Allow-Origin":["http://localhost:'${IPFS_WEBUI_PORT}'", "http://127.0.0.1:'${IPFS_WEBUI_PORT}'", "http://'${HOSTNAME}':'${IPFS_WEBUI_PORT}'"],
-    "Access-Control-Allow-Methods":["PUT", "POST"]}'
-ipfs config --json Routing.Type '"dht"'
+    '{
+        "Access-Control-Allow-Origin": ["http://localhost:'${IPFS_WEBUI_PORT}'", "http://127.0.0.1:'${IPFS_WEBUI_PORT}'", "http://'${HOSTNAME}':'${IPFS_WEBUI_PORT}'"],
+        "Access-Control-Allow-Methods": ["PUT", "POST"]
+    }'
+ipfs config --json Routing.Type '"none"'
 ipfs config --json Swarm.DisableNatPortMap true
 ipfs config --json Swarm.Transports.Network.Websocket false
 ipfs config --json Gateway.NoFetch true
+ipfs config --json DNS.Resolvers '{}'
+ipfs config --json Routing.DelegatedRouters '[]'
+ipfs config --json Ipns.DelegatedPublishers '[]'
 ipfs bootstrap rm --all
